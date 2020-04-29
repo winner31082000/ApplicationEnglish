d2ffd1898ad8f0fa66ddf3687edeed086a80f881
Vũ Long Giang <46051565+VLGIN@users.noreply.github.com>
2020-04-29 17:56:29 +0700

    ready project

--- src/sample/MConnection.java
+++ src/sample/MConnection.java
@@ -7,13 +7,8 @@ import java.sql.SQLException;
 public class MConnection {
 //    private final String DBNAM = "Tutorial";
     private final String USER = "sa";
-<<<<<<< HEAD
-    private final String PASS = "23571113";
-    private final String MURL = "jdbc:sqlserver://localhost:1433;databaseName=dk";
-=======
     private final String PASS = "20102000";
     private final String MURL = "jdbc:sqlserver://localhost:1433;databaseName=EnglishApp";
->>>>>>> 85261fde35d10e79f1e5192cfdbb202d8311934e
 
     private static MConnection instance = new MConnection();
     public MConnection(){}
